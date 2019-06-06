198070780460076f428fc59e5ae45cd2bec60d65
rmanicav <rmanicavasagam@gmail.com>
2019-06-06 12:12:10 -0500

    Datatype changes

---
+++ Source/limiter.cpp
@@ -0,0 +1,194 @@
+#include "../Include/limiter.h"
+
+// Function Definitions
+
+// Function Definitions
+
+//
+// Arguments    : double n
+//                const double f[1764]
+//                emxArray_real_T *df1
+//                emxArray_real_T *df2
+// Return Type  : void
+//
+void flimiter(double n, double f[1764], double df1[1764],
+	double df2[1764])
+{
+	int k;
+	int loop_ub;
+	double df1x;
+	double df2x;
+	double df1y;
+	double df2y;
+	double s;
+	double a;
+	double varargin_1[3];
+	int idx;
+	int b_k;
+	bool exitg1;
+	
+	//emxEnsureCapacity_real_T(df1, k);
+	loop_ub = (int)n * (int)n;
+	for (k = 0; k < loop_ub; k++) {
+		df1[k] = 0.0;
+		df2[k] = 0.0;
+	}
+	
+	
+
+	/*for (k = 0; k < (int)n; k++) {
+		df1->data[((int)n + df1->size[0] * k) - 1] = 0.0;
+		df1->data[k + df1->size[0] * ((int)n - 1)] = 0.0;
+		df2->data[((int)n + df2->size[0] * k) - 1] = 0.0;
+		df2->data[k + df2->size[0] * ((int)n - 1)] = 0.0;
+	}*/
+
+	for (loop_ub = 1; loop_ub - 1 < (int)((n - 1.0) + -1.0); loop_ub++) {
+		for (k = 1; k - 1 < (int)((n - 1.0) + -1.0); k++) {
+			df1x = f[((int)((2.0 + (double)(loop_ub - 1)) + 1.0) + 42 * k) - 1] -
+				f[loop_ub + 42 * k];
+			df2x = f[loop_ub + 42 * k] - f[((int)((2.0 + (double)(loop_ub - 1)) - 1.0)
+				+ 42 * k) - 1];
+			df1y = f[loop_ub + 42 * ((int)((2.0 + (double)(k - 1)) + 1.0) - 1)] -
+				f[loop_ub + 42 * k];
+			df2y = f[loop_ub + 42 * k] - f[loop_ub + 42 * ((int)((2.0 + (double)(k - 1))
+				- 1.0) - 1)];
+
+			//  Superbee limiterv  (df1x,df2x,df1m,df1y,df2y,df2m)
+			if (df1x * df2x < 0.0) {
+				df2x = 0.0;
+			}
+			else {
+				s = df1x;
+				if (df1x < 0.0) {
+					s = -1.0;
+				}
+				else if (df1x > 0.0) {
+					s = 1.0;
+				}
+				else {
+					if (df1x == 0.0) {
+						s = 0.0;
+					}
+				}
+
+				a = std::abs(df1x);
+				df1x = std::abs(df2x);
+				varargin_1[0] = 2.0 * a;
+				varargin_1[1] = 2.0 * df1x;
+				varargin_1[2] = 0.5 * (a + df1x);
+				if (varargin_1[0] != NAN) {
+					idx = 1;
+				}
+				else {
+					idx = 0;
+					b_k = 2;
+					exitg1 = false;
+					while ((!exitg1) && (b_k < 4)) {
+						if (varargin_1[b_k - 1] != NAN) {
+							idx = b_k;
+							exitg1 = true;
+						}
+						else {
+							b_k++;
+						}
+					}
+				}
+
+				if (idx == 0) {
+					df1x = varargin_1[0];
+				}
+				else {
+					df1x = varargin_1[idx - 1];
+					while (idx + 1 < 4) {
+						if (df1x > varargin_1[idx]) {
+							df1x = varargin_1[idx];
+						}
+
+						idx++;
+					}
+				}
+
+				df2x = s * df1x;
+			}
+
+			if (df1y * df2y < 0.0) {
+				df1x = 0.0;
+			}
+			else {
+				s = df1y;
+				if (df1y < 0.0) {
+					s = -1.0;
+				}
+				else if (df1y > 0.0) {
+					s = 1.0;
+				}
+				else {
+					if (df1y == 0.0) {
+						s = 0.0;
+					}
+				}
+
+				a = std::abs(df1y);
+				df1x = std::abs(df2y);
+				varargin_1[0] = 2.0 * a;
+				varargin_1[1] = 2.0 * df1x;
+				varargin_1[2] = 0.5 * (a + df1x);
+				if (varargin_1[0] != NAN) {
+					idx = 1;
+				}
+				else {
+					idx = 0;
+					b_k = 2;
+					exitg1 = false;
+					while ((!exitg1) && (b_k < 4)) {
+						if (varargin_1[b_k - 1] !=NAN) {
+							idx = b_k;
+							exitg1 = true;
+						}
+						else {
+							b_k++;
+						}
+					}
+				}
+
+				if (idx == 0) {
+					df1x = varargin_1[0];
+				}
+				else {
+					df1x = varargin_1[idx - 1];
+					while (idx + 1 < 4) {
+						if (df1x > varargin_1[idx]) {
+							df1x = varargin_1[idx];
+						}
+
+						idx++;
+					}
+				}
+
+				df1x *= s;
+			}
+
+			//
+			//  if(df1x*df2x < 0),
+			//          f1=0;
+			//    else
+			//          s=sign(df1x);
+			//          a=abs(df1x);
+			//          b=abs(df2x);
+			//          f1=s*min(max([a b]),2.0*min([a b]));
+			//  end
+			//
+			//  if(df1y*df2y < 0),
+			//          f2=0;
+			//    else
+			//          s=sign(df1y);
+			//          a=abs(df1y);
+			//          b=abs(df2y);
+			//          f2=s*min(max([a b]),2.0*min([a b]));
+			//  end
+			df1[loop_ub] = df2x;
+			df2[loop_ub] = df1x;
+		}
+	}
+}
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,68 +1,74 @@
 #include<iostream>
 #include<fstream>
 #include<ostream>
-#include "../Include/corrector.h"
-#include "../Include/limit.h"
-#include "../Include/limiter.h"
-#include "../Include/predictor.h"
-#include "../Include/slope.h"
-#include "../Include/solver.h"
-#include "../Include/fluxes.h"
+#include "corrector.cpp"
+#include "limiter.cpp"
+#include "predictor.cpp"
+#include "slope.cpp"
+#include "fluxes.cpp"
 using namespace std;
 
 int main(void)
 {
 	int i0;
 	int j;
-	static double zc[1764];
+	double zc[1764];
 	int k;
-	static double wse[1764];
-	static const signed char iv0[31] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+	double wse[1764];
+	const signed char iv0[31] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
 	  13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 34, 35, 36, 37, 38, 39, 40, 41 };
 
-	static double U[5292];
-	static double h[1764];
-	static double u[1764];
-	static double v[1764];
+	double U[5292];
+	double h[1764];
+	double u[1764];
+	double v[1764];
 	int simtime;
-	emxArray_real_T *dzcx;
-	emxArray_real_T *dzcy;
-	emxArray_real_T *dwsex;
-	emxArray_real_T *dwsey;
-	emxArray_real_T *dux;
-	emxArray_real_T *duy;
-	emxArray_real_T *dvx;
-	emxArray_real_T *dvy;
-	//double sox[1764];
-	//double soy[1764];
-	//double sfx[100];
-	//double sfy[100];
+	//emxArray_real_T *dzcx;
+	double dzcx[1764];
+	double dzcy[1764];
+	double dwsex[1764];
+	double dwsey[1764]; 
+	double dux[1764];
+	double duy[1764];
+	double dvx[1764];
+	double dvy[1764];
+	double sox[1764];
+	double soy[1764];
+	double sfx[100];
+	double sfy[100];
 	static int iv1[2] = { 10, 10 };
 
-	//double wsep[1764];
-	//double up[1764];
-	//double vp[1764];
+	double wsep[1764];
+	double up[1764];
+	double vp[1764];
 	//signed char fileid;
 	static double unusedExpr[5292];
 	int n = 42;
 	//
 	double grav = 9.806;
+	double ManN = 0.001;
+	
+	double hextra = 0.1;
+	double epsilon = 0.1;// minimum depth of water(threshold for dry bed)(1cm)
+	int	cellsize = 5;// Cell size(dx = dy)m
+	int	L = 210;// box size(200 * 200m)
+	n = L / cellsize; // (n = 40) number of cells(nx = ny)
+	int	m = n;
+	int nf = n;// number of edges(will be used to compute fluxes at the interface)
+	double	cr = 0.05;
 
-	//  ManN=0.00;
-	//  minimum depth of water (threshold for dry bed)(1cm)
-	//  Cell size(dx=dy)m
-	//  box size(200*200m)
-	//  (n=40) number of cells (nx=ny)
-	//  number of edges   (will be used to compute fluxes at the interface)                     
-	//  x=0:cellsize:L;                         % array for edge coordinates in x    
-	//  y=0:cellsize:L;                         % array for edge coordinates in y
-	//  x coordinate of cell center
-	//  y coordinate of cell center
-	//  number of time steps
-	//  plotting interval
-	//  time steps
-	//  ratio of dt/dx or dt/dy
-	//  Define bed elevation
+	// x = 0:cellsize:L;// array for edge coordinates in x
+		// y = 0:cellsize:L;// array for edge coordinates in y
+
+	double xc = cellsize / 2; //cellsize:L;// x coordinate of cell center
+	double	yc = cellsize / 2; //:cellsize:L;// y coordinate of cell center
+
+	double	nt = 20000;// number of time steps
+	double	ntplot = 2;// plotting interval
+	double	dt = 0.05;// time steps
+	double	dt2 = dt / cellsize;// ratio of dt / dx or dt / dy
+	double hp = 0.0;
+	double amax = 0.0;
 	for (i0 = 0; i0 < 1764; i0++) {
 		zc[i0] = 1.0;
 	}
@@ -126,7 +132,7 @@ int main(void)
 	}
 
 	// Intialize the arrays
-	memset(&U[0], 0, 5292U * sizeof(double));
+	std::memset(&U[0], 0, 5292U * sizeof(double));
 
 	//  fluxes in the $x$ direction
 	//  fluxes in the $y$ direction
@@ -158,28 +164,30 @@ int main(void)
 	//  tcntr=0;
 	for (j = 0; j < 20000; j++) {
 		simtime = 1 + j;
-		limiter(n,zc, dzcx, dzcy);
-		limiter(n,wse, dwsex, dwsey);
-		limiter(n,u, dux, duy);
-		limiter(n,v, dvx, dvy);
+
+		flimiter(n,zc, dzcx, dzcy);
+		flimiter(n,wse, dwsex, dwsey);
+		flimiter(n,u, dux, duy);
+		flimiter(n,v, dvx, dvy);
 
 		//  Slope calculation
-		//slope(h, u, v, dzcx, dzcy, sox, soy, sfx, sfy);
+		fslope(h, u, v, ManN, hextra, dzcx, dzcy, cellsize, n, sox, soy, sfx, sfy);
+		
 
 		//  predictor step (estimate the values at half timestep)
-		//predictor(wse, h, u, v, dwsex, dwsey, dux, duy, dvx, dvy, dzcx, dzcy, zc,
-			//sox, sfx, iv1, soy, sfy, iv1, wsep, up, vp);
+		fpredictor(nf,wse,h,u,v,dwsex,dwsey,dux,duy,dvx,dvy,dt2,dzcx,dzcy,epsilon,zc,sox,sfx,dt,soy,sfy,wsep,up,vp);
+		
 
 		//    Compute fluxes at the interfaces
-		// fluxes(UP,n,dwsex,dwsey,dux,duy,dvx,dvy,hextra,zc);
+		// ffluxes(UP,n,dwsex,dwsey,dux,duy,dvx,dvy,hextra,zc);
 		//  Estimate the flux vectors on the next time step
-		//corrector(U, sox, sfx, iv1, soy, sfy, iv1, grav, unusedExpr);
+		//fcorrector(U, sox, sfx, iv1, soy, sfy, iv1, grav, unusedExpr);
 
 		//  U(:,:,:)=Unew(:,:,:);
 		//  h(:,:)= Unew(:,:,1);                    % computed water depth (water level)      
 		//  if Unew(:,:,1)< epsilon
-		memset(&u[0], 0, 1764U * sizeof(double));
-		memset(&v[0], 0, 1764U * sizeof(double));
+		std::memset(&u[0], 0, 1764U * sizeof(double));
+		std::memset(&v[0], 0, 1764U * sizeof(double));
 
 		// else
 		// u(:,:)= Unew(:,:,2)./(Unew(:,:,1)+hextra);
@@ -190,32 +198,42 @@ int main(void)
 	// if wse < 0
 	//    wse = 0;
 	// end
-	//      tcntr=tcntr+dt;
-	//    dt= cr*cellsize/amax;
-	//   cr=amax*dt/cellsize;
+	   //t=t+dt;
+	   dt= cr*cellsize/amax;
+	   cr=amax*dt/cellsize;
 	//  Writes the output at each counters seconds
-	
-	/*if (mod((double)simtime * 0.05) == 0.0) {
-		//fileid.open("C:\\Users\\raj\\source\\repos\\Flood2dOutput\\hOut_","w");
-
+	   double ctrs = simtime * dt;
+	   ofstream hfile;
+	if (fmod(ctrs,ntplot) == 0.0) {
+		hfile.open("C:/Users/raj/source/repos/Flood2dOutput/hOut_",ios::out);
 		//    fprintf(fid1,'%.2f\r\n',h(:,:));
 		//   dlmwrite(outfile,x,'delimiter','\t','precision',12)
-		//fileid.close();
+		hfile.close();
 
 		//  % %   outputs the 2D-water depth plot
 		//  ctrs=sprintf('L%s',ctrs);
 		// saveas(gca,output);
 	}
-
-	if (mod((double)simtime * 0.05) == 0.0) {
-		//fileid1.open("C:/Users/raj/source/repos/Flood2d/Output/hsensor1.txt","w");
-		fileid1.close();
+	double ctrs2 = simtime * dt;
+	if (fmod(ctrs2,0.5) == 0) {
+		//fileid.open("C:/Users/raj/source/repos/Flood2d/Output/hsensor1.txt","w");
+		fileid.close();
 
 		// dlmwrite(outfile2,hsens_1,'-append','newline','pc','delimiter','\t','precision',12) 
 		// ctrs2=num2str(ctrs2);
 	}
+	double ctrs3= simtime * dt;
+	if (fmod(ctrs3, 0.5) == 0) {
+		//fileid2.open("C:\Users\raj\source\repos\Flood2dOutput\hsensor2.txt","w");
 
-	if (mod((double)simtime * 0.05) == 0.0) {
+		// dlmwrite(outfile3,hsens_2,'-append','newline','pc','delimiter','\t','precision',12) 
+		fileid1.close();
+
+		//  ctrs3=num2str(ctrs3);
+	}
+
+	double ctrs4 = simtime * dt;
+	if (fmod(ctrs4, 0.5)) {
 		//fileid2.open("C:\Users\raj\source\repos\Flood2dOutput\hsensor2.txt","w");
 
 		// dlmwrite(outfile3,hsens_2,'-append','newline','pc','delimiter','\t','precision',12) 
@@ -223,15 +241,6 @@ int main(void)
 
 		//  ctrs3=num2str(ctrs3);
 	}
-
-	if (mod((double)simtime * 0.05) == 0.0) {
-		//fileid3.open("C:\\Users\\raj\\source\\repos\\Flood2dOutput\\hsensor3.txt","w");
-
-		// dlmwrite(outfile4,hsens_3,'-append','newline','pc','delimiter','\t','precision',12) 
-		fileid3.close();
-
-		// ctrs4=num2str(ctrs4);
-	}*/
 
 	return 0;
 }
