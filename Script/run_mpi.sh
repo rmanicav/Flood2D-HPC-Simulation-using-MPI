f3df43b35c859b4f1799251da936f3ffec563ffb
rmanicav <44268387+rmanicav@users.noreply.github.com>
2026-01-23 17:07:50 +0530

    Update run_mpi.sh

--- Script/run_mpi.sh
+++ Script/run_mpi.sh
@@ -1,1 +1,5 @@
 # Example MPI execution script for Flood2D simulation
+module load gcc/6.3.0
+#g++ corrector.cpp fluxes.cpp limiter.cpp main.cpp predictor.cpp slope.cpp solver.cpp 
+mpic++ -o main  corrector.cpp fluxes.cpp limiter.cpp main.cpp predictor.cpp slope.cpp solver.cpp -lm -fopenmp
+mpirun -n 2 ./main
